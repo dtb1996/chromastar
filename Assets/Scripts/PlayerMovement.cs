b496ee76d23206dc85fb46ac963bb960d144f19f
Dillon <dtb1996@yahoo.com>
2022-01-30 20:13:50 -0500

    added dash ability and fixed attack collision bug

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,20 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     public float jumpHeight;
     public float midAirControl;
+    public float dashCooldown = 1f;
     public AudioClip jumpSFX;
     public AudioClip attackSFX;
     public AudioSource sfxPlayer;
 
+    //Dash ability variables
+    public float dashDistance = 15f;
+    private bool isDashing;
+    private float timeSinceLastDash;
+    //private float doubleTapTime;
+    //private KeyCode lastKeyCode;
+
     private bool isMoving;
+    private bool isFacingRight;
     private Vector2 input;
 
     private float coyoteTime = 0.1f;
@@ -42,6 +51,57 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         HandleJump();
+
+        timeSinceLastDash -= Time.deltaTime;
+
+        if (!isDashing && timeSinceLastDash <= 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                if (isFacingRight)
+                {
+                    StartCoroutine(Dash(1f));
+                    timeSinceLastDash = dashCooldown;
+                }
+                else
+                {
+                    StartCoroutine(Dash(-1f));
+                    timeSinceLastDash = dashCooldown;
+                }
+                
+            }
+
+            ////Dash left
+            //if (Input.GetKeyDown(KeyCode.A))
+            //{
+            //    if (doubleTapTime > Time.time && lastKeyCode == KeyCode.A)
+            //    {
+            //        StartCoroutine(Dash(-1f));
+            //    }
+            //    else
+            //    {
+            //        doubleTapTime = Time.time + 0.3f;
+            //    }
+
+            //    lastKeyCode = KeyCode.A;
+            //}
+
+            ////Dash right
+            //if (Input.GetKeyDown(KeyCode.D))
+            //{
+            //    if (doubleTapTime > Time.time && lastKeyCode == KeyCode.D)
+            //    {
+            //        StartCoroutine(Dash(1f));
+            //    }
+            //    else
+            //    {
+            //        doubleTapTime = Time.time + 0.3f;
+            //    }
+
+            //    lastKeyCode = KeyCode.D;
+            //}
+        }
+        
 
         //if (IsGrounded() && rb.velocity.y == 0)
         //{
@@ -74,20 +134,44 @@ public class PlayerMovement : MonoBehaviour
         //{
         //    HandleMovement();
         //}
-        HandleMovement();
+        if (!isDashing)
+        {
+            HandleMovement();
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                //StartCoroutine("Attack");
+
+                animator.SetBool("isAttacking", true);
+
+                CapsuleCollider2D collider = attackCollider.GetComponent<CapsuleCollider2D>();
+
+                if (isFacingRight)
+                {
+                    if (collider.offset.x < 0f)
+                    {
+                        collider.offset = new Vector2(-(collider.offset.x), collider.offset.y);
+                    }
+                }
+                else
+                {
+                    if (collider.offset.x > 0f)
+                    {
+                        collider.offset = new Vector2(-(collider.offset.x), collider.offset.y);
+                    }
+                }
+
+                attackCollider.SetActive(true);
+
+                sfxPlayer.clip = attackSFX;
+                sfxPlayer.Play();
+            }
+        }
+        //HandleMovement();
 
         HandleAnimation();
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            //StartCoroutine("Attack");
-
-            animator.SetBool("isAttacking", true);
-            attackCollider.SetActive(true);
-            
-            sfxPlayer.clip = attackSFX;
-            sfxPlayer.Play();
-        }
+        
         //else if (Input.GetMouseButtonUp(0))
         //{
         //    animator.SetBool("isAttacking", false);
@@ -130,14 +214,14 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey("a"))
         {
-            spriteRenderer.flipX = true;
+            isFacingRight = false;
             rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
         }
         else
         {
             if (Input.GetKey("d"))
             {
-                spriteRenderer.flipX = false;
+                isFacingRight = true;
                 rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
             }
             else
@@ -186,8 +270,33 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private IEnumerator Dash(float direction)
+    {
+        isDashing = true;
+        //rb.velocity = new Vector2(rb.velocity.x * 0.5f, 0f);
+        rb.velocity = new Vector2(rb.velocity.x * 0.2f, 0f);
+        rb.AddForce(new Vector2(dashDistance * direction, 0f), ForceMode2D.Impulse);
+        float gravity = rb.gravityScale;
+        if (IsGrounded())
+        {
+            rb.gravityScale = gravity * 0.4f;
+        }
+        else
+        {
+            rb.gravityScale = 0f;
+        }
+        
+
+        yield return new WaitForSeconds(0.4f);
+
+        isDashing = false;
+        rb.gravityScale = gravity;
+    }
+
     private void HandleAnimation()
     {
+        spriteRenderer.flipX = !isFacingRight;
+
         if (IsGrounded())
         {
             animator.SetBool("isJumping", false);
